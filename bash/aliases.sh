945d440f1ea311fc0970b352a4ca94d336585406
Anthony Smith <anthony.smith@2x2.dev>
2024-06-26 13:59:05 -0700

    Default initial binds for aerc

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -4,4 +4,4 @@ alias nvim="nvim -u $DOTFILES_LOCATION/nvim/init.lua"
 
 # Always enforce that aerc uses the custom configuration file located in
 # $DOTFILES_LOCATION/aerc/aerc.conf
-alias aerc="aerc --aerc-conf $DOTFILES_LOCATION/aerc/aerc.conf"
+alias aerc="aerc --aerc-conf $DOTFILES_LOCATION/aerc/aerc.conf --binds-conf $DOTFILES_LOCATION/aerc/binds.conf"
