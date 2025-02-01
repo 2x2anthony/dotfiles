0868cc797ea2899cbe36ab6d0b48a89c19ed2795
Anthony Smith <anthony.smith@2x2.dev>
2025-02-01 08:38:30 -0800

    Don't use set-option. Call bash directly as session is created.

--- bash/environments/dev-esp.sh
+++ bash/environments/dev-esp.sh
@@ -2,8 +2,7 @@ function dev-esp {
     local ESP_SESSION_NAME="TmuxEspDevelopmentEnvironment"
     tmux has-session -t $ESP_SESSION_NAME 2>&1 >/dev/null
     if [[ 0 != $? ]]; then
-        tmux -f $TMUX_CONFIGURATION new -d -s $ESP_SESSION_NAME -n "ESP-IDF Environment" -c "/work/embedded"
-        tmux set-option -s -t $ESP_SESSION_NAME default-command "bash --init-file $DOTFILES_LOCATION/bash/environments/_dev-esp-environment.sh"
-        exec tmux attach-session -t $ESP_SESSION_NAME
+        tmux -f $TMUX_CONFIGURATION new-session -d -s $ESP_SESSION_NAME -n "ESP-IDF Environment" -c "/work/embedded" "bash --init-file $DOTFILES_LOCATION/bash/environments/_dev-esp-environment.sh"
+        tmux attach-session -t $ESP_SESSION_NAME
     fi
 }
