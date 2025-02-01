dc3f7e21a191c26fe94cbba9df38b0947df00e5c
Anthony Smith <anthony.smith@2x2.dev>
2025-02-01 08:39:02 -0800

    Re-add exec to tmux call for ESP environment

--- bash/environments/dev-esp.sh
+++ bash/environments/dev-esp.sh
@@ -3,6 +3,6 @@ function dev-esp {
     tmux has-session -t $ESP_SESSION_NAME 2>&1 >/dev/null
     if [[ 0 != $? ]]; then
         tmux -f $TMUX_CONFIGURATION new-session -d -s $ESP_SESSION_NAME -n "ESP-IDF Environment" -c "/work/embedded" "bash --init-file $DOTFILES_LOCATION/bash/environments/_dev-esp-environment.sh"
-        tmux attach-session -t $ESP_SESSION_NAME
+        exec tmux attach-session -t $ESP_SESSION_NAME
     fi
 }
