f472af863b9231edad7d8476cbe6b97fb643a924
Anthony Smith <anthony.smith@2x2.dev>
2025-01-31 21:44:30 -0800

    Add some aliases centered around idf.py for ESP32 development

--- bash/environments/_dev-esp-environment.sh
+++ bash/environments/_dev-esp-environment.sh
@@ -1,3 +1,50 @@
 source ~/.bashrc
 source /work/SDKs/esp-idf-$ESP_IDF_VER/export.sh
 
+function set-target {
+    idf.py set-target "$@"
+}
+
+function menuconfig {
+    idf.py menuconfig "$@"
+}
+
+function build {
+    idf.py build "$@"
+}
+
+function flash {
+    if [[ "" == "$1" ]]; then
+        echo "Expected a value for the ESP32 device port as the first argument"
+        return
+    fi
+    idf.py -p $1 flash
+}
+
+function flash-erase {
+    if [[ "" == "$1" ]]; then
+        echo "Expected a value for the ESP32 device port as the first argument"
+        return
+    fi
+
+    idf.py -p $1 erase-flash
+}
+
+function otadata-erase {
+    if [[ "" == "$1" ]]; then
+        echo "Expected a value for the ESP32 device port as the first argument"
+        return
+    fi
+
+    idf.py -p $1 erase-otadata
+}
+
+function monitor {
+    if [[ "" == "$1" ]]; then
+        echo "Expected a value for the ESP32 device port as the first argument"
+        return
+    fi
+
+    idf.py -p $1 monitor
+}
+
